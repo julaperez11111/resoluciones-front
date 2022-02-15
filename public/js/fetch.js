15ed43219aca7fa0fd054e03d67b1f68b89e238a
julaperez11111 <julaperez11111@misena.edu.co>
2022-02-14 21:48:33 -0500

    se agrega experiencia de usuario con modal y limpieza de campos

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,4 +1,4 @@
-let request = {
+const defaultRequest = {
   nombres: "",
   nacionalidad: "",
   numUnico: "",
@@ -14,17 +14,23 @@ let request = {
   valorMulta: "",
 };
 
+let request = Object.assign(defaultRequest);
+
 let state = {
   loading: false,
   error: null,
 };
 
-const mainContainer = document.getElementById("main-container")
+const mainContainer = document.getElementById("main-container");
 
-const spinner = document.getElementById("spinner")
+const spinner = document.getElementById("spinner");
+
+const modal = document.getElementById("modal");
 
 // Este es el botón imprimir
 const submit = document.getElementById("submit");
+
+const actionButton = document.getElementById("action-button");
 
 // Este es el select
 const valorMulta = document.getElementById("valorMulta");
@@ -49,9 +55,9 @@ document.querySelectorAll("input").forEach((item) => {
 });
 
 const postData = async (request) => {
-  toggleSpinner(true)
+  toggleSpinner(true);
   const body = request;
-  console.log(body)
+  console.log(body);
   try {
     const res = await fetch("http://localhost:3000/api", {
       method: "POST",
@@ -62,29 +68,74 @@ const postData = async (request) => {
     });
     const data = await res.json();
     state = { loading: false, error: null };
-    toggleSpinner(false)
-    console.log(data);
+    toggleSpinner(false);
+    toggleModal({ typeMessage: "success" });
   } catch (error) {
     console.log(error);
     state = { loading: false, error };
-    toggleSpinner(false)
+    toggleSpinner(false);
+    toggleModal({ typeMessage: "failure", message: error.message });
   }
 };
 
-submit.addEventListener("click", (e) => {
-  e.preventDefault();
-  postData(request);
-}, false);
+submit.addEventListener(
+  "click",
+  (e) => {
+    e.preventDefault();
+    postData(request);
+  },
+  false
+);
 
 const toggleSpinner = (loading) => {
   if (loading) {
-    mainContainer.classList.add("opaque")
+    mainContainer.classList.add("opaque");
     spinner.classList.remove("hidden");
-    spinner.classList.add("visible")
-  }
-  else {
-    mainContainer.classList.remove("opaque")
+    spinner.classList.add("visible");
+  } else {
+    mainContainer.classList.remove("opaque");
     spinner.classList.remove("visible");
-    spinner.classList.add("hidden")
+    spinner.classList.add("hidden");
   }
-}
+};
+
+const toggleModal = (options = {}) => {
+  if (options.typeMessage === "success") {
+    document.getElementById("modal-title").innerHTML = "Se creo exitosamente";
+    document.getElementById("modal-description").innerHTML =
+      "Se generó los archivos correctamente, por favor verifique la carpeta de outputs";
+    mainContainer.classList.add("opaque");
+    modal.classList.remove("hidden");
+    modal.classList.add("visible");
+    return;
+  } else if (options.typeMessage == "failure") {
+    document.getElementById("modal-title").innerHTML = "Ocurrió un error";
+    document.getElementById("modal-description").innerHTML = options.message;
+    mainContainer.classList.add("opaque");
+    modal.classList.remove("hidden");
+    modal.classList.add("visible");
+    return;
+  }
+  mainContainer.classList.remove("opaque");
+  modal.classList.remove("visible");
+  modal.classList.add("hidden");
+  cleanForm();
+};
+
+const cleanForm = () => {
+  request = Object.assign(defaultRequest);
+  document.querySelectorAll("input").forEach((item) => {
+    item.value = ""
+  });
+  valorMulta.value = "Seleccione valor Multa a Aplicar"
+  tipoID.value = "Seleccione el Tipo de ID"
+};
+
+actionButton.addEventListener(
+  "click",
+  (e) => {
+    e.preventDefault();
+    toggleModal();
+  },
+  false
+);
